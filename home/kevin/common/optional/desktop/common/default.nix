8859088757788f5f00c1085def1582baec6fcd2d
nikevsoft <26445418+nikevsoft@users.noreply.github.com>
2024-04-10 21:17:10 -0600

    add gnome-terminal

--- home/kevin/common/optional/desktop/common/default.nix
+++ home/kevin/common/optional/desktop/common/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     ./firefox.nix
+    ./gnome-terminal.nix
   ];
 }
---
+++ home/kevin/common/optional/desktop/common/gnome-terminal.nix
@@ -0,0 +1,13 @@
+{
+	programs.gnome-terminal = {
+		enable = true;
+		profile."250a08f6-6ade-4a8e-905c-ea672e202a4d" = {
+			default = true;
+			visibleName = "Default";
+			audibleBell = false;
+			allowBold = true;
+			boldIsBright = true;
+		};
+		themeVariant = "system";
+	};
+}
